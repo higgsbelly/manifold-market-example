ef48ec545f868fe79657fbeb8f315c7379c58828
higgsbelly <higgsbelly@gmail.com>
2023-08-23 14:52:57 -0500

    2nd commit

--- components/Listing.js
+++ components/Listing.js
@@ -1,7 +1,7 @@
 // a simple listing component
 export default function Listing({ network, id }) {
   return (<div
-    data-widget="m-layout-complete-listing"
+    data-widget="m-card-countdown"
     data-id={id}
     data-network={network}
   ></div>)
